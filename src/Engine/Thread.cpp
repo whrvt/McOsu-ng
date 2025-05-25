faba1923ff32d98f45ab3809ceea7c6bec62d81e
William Horvath <william@horvath.blog>
2025-05-24 21:08:16 -0700

    clean up thread

--- src/Engine/Thread.cpp
+++ src/Engine/Thread.cpp
@@ -1,4 +1,4 @@
-//================ Copyright (c) 2020, PG, All rights reserved. =================//
+//================ Copyright (c) 2025, WH, All rights reserved. =================//
 //
 // Purpose:		thread wrapper
 //
@@ -6,61 +6,7 @@
 //===============================================================================//
 
 #include "Thread.h"
-#include "ConVar.h"
-#include "Engine.h"
-
-#include <thread>
-
-// std::thread implementation of Thread
-class StdThread : public BaseThread
-{
-public:
-#ifdef __EXCEPTIONS
-	StdThread(McThread::START_ROUTINE start_routine, void *arg) : BaseThread()
-#else
-	StdThread(McThread::START_ROUTINE start_routine, void *arg) noexcept : BaseThread()
-#endif
-	{
-		m_bReady = false;
-#ifdef __EXCEPTIONS
-		try
-		{
-#endif
-			m_thread = std::thread(start_routine, arg);
-			m_bReady = true;
-#ifdef __EXCEPTIONS
-		}
-		catch (const std::system_error& e)
-		{
-			if (McThread::debug->getBool())
-				debugLog("StdThread Error: std::thread constructor exception: {:s}\n", e.what());
-		}
-#endif
-	}
-
-	~StdThread() override
-	{
-		if (!m_bReady) return;
-
-		m_bReady = false;
-
-		if (m_thread.joinable())
-			m_thread.join();
-	}
-
-	[[nodiscard]] inline bool isReady() const override {return m_bReady;}
-
-private:
-	std::thread m_thread;
-	bool m_bReady;
-};
 
 ConVar debug_thread("debug_thread", false, FCVAR_NONE);
 
 ConVar *McThread::debug = &debug_thread;
-
-McThread::McThread(START_ROUTINE start_routine, void *arg)
-{
-	m_baseThread = new StdThread(start_routine, arg);
-}
-
--- src/Engine/Thread.h
+++ src/Engine/Thread.h
@@ -1,4 +1,4 @@
-//================ Copyright (c) 2020, PG, All rights reserved. =================//
+//================ Copyright (c) 2025, WH, All rights reserved. =================//
 //
 // Purpose:		thread wrapper
 //
@@ -9,17 +9,10 @@
 #ifndef THREAD_H
 #define THREAD_H
 
-class ConVar;
+#include "ConVar.h"
+#include <thread>
 
-class BaseThread
-{
-public:
-	virtual ~BaseThread() {;}
-
-	[[nodiscard]] virtual bool isReady() const = 0;
-};
-
-class McThread
+class McThread final
 {
 public:
 	static ConVar *debug;
@@ -27,13 +20,44 @@ public:
 	typedef void *(*START_ROUTINE)(void*);
 
 public:
-	McThread(START_ROUTINE start_routine, void *arg);
-	~McThread() {if (m_baseThread) { delete (m_baseThread); (m_baseThread) = nullptr; }}
+#ifdef __EXCEPTIONS
+	McThread(START_ROUTINE start_routine, void *arg)
+#else
+	McThread(START_ROUTINE start_routine, void *arg) noexcept
+#endif
+	{
+		m_bReady = false;
+#ifdef __EXCEPTIONS
+		try
+		{
+#endif
+			m_thread = std::thread(start_routine, arg);
+			m_bReady = true;
+#ifdef __EXCEPTIONS
+		}
+		catch (const std::system_error& e)
+		{
+			if (McThread::debug->getBool())
+				debugLog("McThread Error: std::thread constructor exception: {:s}\n", e.what());
+		}
+#endif
+	}
 
-	[[nodiscard]] inline bool isReady() const {return m_baseThread != nullptr && m_baseThread->isReady();};
+	~McThread()
+	{
+		if (!m_bReady) return;
+
+		m_bReady = false;
+
+		if (m_thread.joinable())
+			m_thread.join();
+	}
+
+	[[nodiscard]] inline bool isReady() const {return m_bReady;}
 
 private:
-	BaseThread *m_baseThread;
+	std::thread m_thread;
+	bool m_bReady;
 };
 
 #endif
