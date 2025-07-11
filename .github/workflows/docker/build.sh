62d427182eb881aade56467dfb5af563e4254b3c
William Horvath <william@horvath.blog>
2025-07-10 19:05:56 -0700

    (TEST) change linux docker/workflow build a bit

--- .github/workflows/docker/build.sh
+++ .github/workflows/docker/build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # Simple script to build McOsu-ng in Docker
 
-# Setting up build directory.. (note: i have no idea what this is doing)
-cd /build
-cp -n -r /src/* ./
-
 echo "Building for target: $HOST"
 set -e
 
 # Building..
-autoreconf
-./configure --disable-system-deps --enable-static --disable-native --with-audio="bass,soloud" --host=$HOST
+./autogen.sh
+cd build-out
+../configure --disable-system-deps --enable-static --disable-native --with-audio="bass,soloud" --host=$HOST
 make -j$(nproc) install
 
 # symlinks turn into copies in .zip files for GHA artifacts, so we need to make a zip of the zip...
